453ae80ff6aa3398d1ec531d7eb80257e9885e00
Johannes Dollinger <emulbreh@googlemail.com>
2013-09-16 23:01:19 +0200

    Added boilerplate .gitignore, setup.py, and LICENCE.txt

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='rdp',
+    version='0.1',
+    description='Recursive Decent Parser Library',
+    author='Johannes Dollinger',
+    author_email='emulbreh@googlemail.com',
+    url='http://github.com/emulbreh/rdp',
+    packages=['rdp'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
